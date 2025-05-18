bb3927ac991c02cb9c77f146993df56136bcb7ec
Stepan Tarabin <upconett@gmail.com>
2025-05-18 21:32:13 +0600

    feat: all resources moved to resources/ and config files autocreation done

--- InnoTester/Utils/Config.py
+++ InnoTester/Utils/Config.py
@@ -7,14 +7,42 @@ from typing import overload
 
 from aiogram.utils.formatting import *
 
+
 if len(sys.argv) == 1:
     print("Docker image file is not specified", file=sys.stderr)
     sys.exit(0)
 
 dockerImageNum = sys.argv[1]
 
-with open("resources/token.yaml") as f:
-    token = yaml.safe_load(f)["token"]
+
+def createTemplateIfNotExists(
+    file_path: str,
+    template: str
+):
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as f:
+            f.write(template)
+
+
+if not os.path.exists("resources"):
+    os.mkdir("resources")
+
+
+createTemplateIfNotExists("resources/token.yaml",
+    "token: \"\""
+)
+
+createTemplateIfNotExists("resources/banlist.yaml",
+    "banned:\n"
+    "- \"user-id\""
+)
+
+createTemplateIfNotExists("resources/moderators.txt",
+    "user-id @username"
+)
+
+
+# Prepared resources (saved in repository)
 
 with open("resources/compile.yaml") as f:
     compileCommands = yaml.safe_load(f)
@@ -22,11 +50,30 @@ with open("resources/compile.yaml") as f:
 with open("resources/messages.yaml") as f:
     messages = yaml.safe_load(f)
 
+
+# Dynamic resources (created only in production)
+
+with open("resources/token.yaml") as f:
+    token = yaml.safe_load(f)["token"]
+    if not token:
+        print("Set token for your bot (./resources/token)")
+        quit(1)
+
+with open("resources/moderators.txt") as f:
+    first_moder = f.readline()
+    if first_moder == "user-id @username":
+        print("Set main moderator (./resources/moderators.txt)")
+        quit(1)
+
+
 with open("resources/banlist.yaml") as f:
     banlist = yaml.safe_load(f)['banned']
+    if "user-id" in banlist:
+        banlist.remove("user-id")
 
 
-def getLanguage(filename, dir = "."):
+
+def getLanguage(filename, dir = "."): # TODO : remove (deprecated)
     for file in os.listdir(dir):
         name = file.split(".")[0]
         extension = file.split(".")[-1]
@@ -109,4 +156,3 @@ def errorHandler(protocol, testCount: int):
                     return Text(f"An error occurred during the testing process. Message: {''.join(protocol[2::])}")
                 case "answer":
                     return Text(f"Got invalid answer from probe program. Message:\n") + Code(''.join(protocol[2::]))
-
